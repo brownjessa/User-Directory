7a4bbf10eb82fe7c6752a1e9f50c97f339262827
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 18:47:10 -0500

    add component

--- src/App.js
+++ src/App.js
@@ -3,3 +3,14 @@ import Wrapper from "./components/Wrapper";
 import EmployeeTable from "./components/EmployeeTable";
 import SearchInput from "./components/SearchInput";
 import "./App.css";
+class App extends Component {
+    state = {
+      users : [],
+      search : ''
+    }
+    componentDidMount() {
+      fetch('https://randomuser.me/api/?results=20&nat=us')
+        .then(res => res.json())
+        .then(data => this.setState({ users : data.results.sort((a,b) => a.name.last > b.name.last ? 1 : -1) }))
+        .catch(err => console.log(err));
+    }
