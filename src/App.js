cb7404d6175bbe7577be6ce188f8248d6bf97dda
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 18:46:22 -0500

    imports for functionality

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import EmployeeTable from "./components/EmployeeTable";
+import SearchInput from "./components/SearchInput";
+import "./App.css";
