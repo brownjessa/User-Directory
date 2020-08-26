843d1f54c63373cde4173160866991f85f0a1b6e
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 18:28:02 -0500

    fill in categories for info

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 const EmployeeTable = (props) => {
-    const {users} = props;
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>FirstName</th>
-                    <th className="sort-option"
-                </tr>
-            </thead>
-        </table>
-    )
-}
+  const { users } = props;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>FirstName</th>
+          <th className="sort-option" onClick={props.handleReverseOrder}>
+            Last Name
+          </th>
+          <th>Address</th>
+          <th>City</th>
+          <th>State</th>
+          <th>State</th>
+          <th>Zip Code</th>
+          <th>email</th>
+        </tr>
+      </thead>
+    </table>
+  );
+};
