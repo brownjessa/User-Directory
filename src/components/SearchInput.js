946407b84c871f02c4294c9ee919b220441b6f2f
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 18:41:15 -0500

    render search input

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,4 +4,19 @@ class SearchInput extends Component {
     input: "",
   };
 }
-onInputChange = value;
+onInputChange = (value) => {
+    this.setState ({ input : value }, ()=>{
+        this.PaymentResponse.handleSearchValue(this.state.input)
+    })
+}
+render () {
+    return (
+        <div>
+            <input
+            type="text"
+            placeholder="Search for Last Name"
+            value={this.state.input}
+            onChange={(e)=> this.onInputChange(e.targetvalue)}/>
+        </div>
+    )
+}
