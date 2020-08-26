57020c4fc5b73d4aeebc2fd088d9bfff9a8ae24b
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 18:25:56 -0500

    begin employee table

---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const EmployeeTable = (props) => {
+    const {users} = props;
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>FirstName</th>
+                    <th className="sort-option"
+                </tr>
+            </thead>
+        </table>
+    )
+}
